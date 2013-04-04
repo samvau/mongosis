0337eba68b1b9ceec8eb1e6563a62123a0484e45
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2013-04-04 13:04:48 +0100

    Adding proper namespace to test classes

--- MongoSsisDataSourceTests/CollectionNameEditorTest.cs
+++ MongoSsisDataSourceTests/CollectionNameEditorTest.cs
@@ -1,133 +1,156 @@
-﻿using MongoDataSource;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MongoDB.Driver;
+﻿/*
+ * Copyright (c) 2012-2013 Xbridge Ltd
+ * See the file license.txt for copying permission.
+ */
+
 using System.Drawing.Design;
-using Telerik.JustMock;
-using System.Windows.Forms.Design;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDataSource;
+using MongoDB.Driver;
+using Telerik.JustMock;
 
-/// <summary>
-///This is a test class for CollectionNameEditorTest and is intended
-///to contain all CollectionNameEditorTest Unit Tests
-///</summary>
-[TestClass()]
-public class CollectionNameEditorTest {
-
-    /// <summary>
-    ///A test for EditValue
-    ///</summary>
-    [TestMethod()]
-    public void EditValueTest() {
-        NeedSealedClassMocks();
-    }
+namespace MongoSourceTests
+{
 
     /// <summary>
-    ///A test for GetDatabase
+    ///This is a test class for CollectionNameEditorTest and is intended
+    ///to contain all CollectionNameEditorTest Unit Tests
     ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void GetDatabaseTest() {
-        NeedSealedClassMocks();
-    }
+    [TestClass()]
+    public class CollectionNameEditorTest
+    {
 
-    /// <summary>
-    ///A test for GetEditStyle
-    ///</summary>
-    [TestMethod()]
-    public void GetEditStyleTest() {
-        CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor(); 
-        Assert.AreEqual(UITypeEditorEditStyle.DropDown, target.GetEditStyle(null));
-    }
-
-    /// <summary>
-    ///A test for GetMongoDBConnectionManager
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void GetMongoDBConnectionManagerFromContextTest() {
-        NeedSealedClassMocks();
-    }
-
-    /// <summary>
-    ///A test for GetMongoDBConnectionManager
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void GetMongoDBConnectionManagerFromPackageTest() {
-        NeedSealedClassMocks();
-    }
-
-    /// <summary>
-    ///A test for GetPackageFromContext
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void GetPackageFromContextTest() {
-        NeedSealedClassMocks();
-    }
-
-    /// <summary>
-    ///A test for OnListBoxSelectedValueChanged
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void OnListBoxSelectedValueChangedTest() {
-        CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor();
-        IWindowsFormsEditorService serviceMock = Mock.Create<IWindowsFormsEditorService>();
-        target.edSvc = serviceMock;
-        Mock.Arrange(() => serviceMock.CloseDropDown()).OccursOnce();
-        target.OnListBoxSelectedValueChanged(null, null);
-        Mock.Assert(serviceMock);
-    }
-
-    /// <summary>
-    ///A test for BuildListBox
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void BuildListBoxTest() {
-        string[] collectionNames = new string[]{"collection1","collection2"};
-
-        ListBox lb = SetUpListBoxForTest(collectionNames);
-
-        Assert.AreEqual(SelectionMode.One, lb.SelectionMode);
-
-        Assert.AreEqual(2, lb.Items.Count);
-        foreach (string name in collectionNames) {
-            Assert.IsTrue(lb.Items.Contains(name));
+        /// <summary>
+        ///A test for EditValue
+        ///</summary>
+        [TestMethod()]
+        public void EditValueTest()
+        {
+            NeedSealedClassMocks();
         }
-    }
 
-    /// <summary>
-    ///A test for BuildListBox
-    ///</summary>
-    [TestMethod()]
-    [DeploymentItem("MongoSsisDataSource.dll")]
-    public void BuildListBoxIgnoresSystemCollectionsTest() {
-       
-        string[] collectionNames = new string[] { "collection1", "collection2", "system_collection" };
-        ListBox lb = SetUpListBoxForTest(collectionNames);
-        
-        Assert.AreEqual(2, lb.Items.Count);
-
-        foreach (string name in collectionNames) {
-            if (name.StartsWith("system")) {
-                Assert.IsFalse(lb.Items.Contains(name));
-            } 
+        /// <summary>
+        ///A test for GetDatabase
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void GetDatabaseTest()
+        {
+            NeedSealedClassMocks();
         }
-    }
 
-    private ListBox SetUpListBoxForTest(string[] names) {
-        CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor();
-        MongoDatabase database = Mock.Create<MongoDatabase>(Constructor.Mocked);
+        /// <summary>
+        ///A test for GetEditStyle
+        ///</summary>
+        [TestMethod()]
+        public void GetEditStyleTest()
+        {
+            CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor();
+            Assert.AreEqual(UITypeEditorEditStyle.DropDown, target.GetEditStyle(null));
+        }
 
-        Mock.Arrange(() => database.GetCollectionNames()).Returns(names);
+        /// <summary>
+        ///A test for GetMongoDBConnectionManager
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void GetMongoDBConnectionManagerFromContextTest()
+        {
+            NeedSealedClassMocks();
+        }
 
-       return target.BuildListBox(database);
-    }
+        /// <summary>
+        ///A test for GetMongoDBConnectionManager
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void GetMongoDBConnectionManagerFromPackageTest()
+        {
+            NeedSealedClassMocks();
+        }
 
-    private void NeedSealedClassMocks() {
-        Assert.Inconclusive("Cannot test without paid-for mocking framework that allows mocking sealed classes.");
+        /// <summary>
+        ///A test for GetPackageFromContext
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void GetPackageFromContextTest()
+        {
+            NeedSealedClassMocks();
+        }
+
+        /// <summary>
+        ///A test for OnListBoxSelectedValueChanged
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void OnListBoxSelectedValueChangedTest()
+        {
+            CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor();
+            IWindowsFormsEditorService serviceMock = Mock.Create<IWindowsFormsEditorService>();
+            target.edSvc = serviceMock;
+            Mock.Arrange(() => serviceMock.CloseDropDown()).OccursOnce();
+            target.OnListBoxSelectedValueChanged(null, null);
+            Mock.Assert(serviceMock);
+        }
+
+        /// <summary>
+        ///A test for BuildListBox
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void BuildListBoxTest()
+        {
+            string[] collectionNames = new string[] { "collection1", "collection2" };
+
+            ListBox lb = SetUpListBoxForTest(collectionNames);
+
+            Assert.AreEqual(SelectionMode.One, lb.SelectionMode);
+
+            Assert.AreEqual(2, lb.Items.Count);
+            foreach (string name in collectionNames)
+            {
+                Assert.IsTrue(lb.Items.Contains(name));
+            }
+        }
+
+        /// <summary>
+        ///A test for BuildListBox
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("MongoSsisDataSource.dll")]
+        public void BuildListBoxIgnoresSystemCollectionsTest()
+        {
+
+            string[] collectionNames = new string[] { "collection1", "collection2", "system_collection" };
+            ListBox lb = SetUpListBoxForTest(collectionNames);
+
+            Assert.AreEqual(2, lb.Items.Count);
+
+            foreach (string name in collectionNames)
+            {
+                if (name.StartsWith("system"))
+                {
+                    Assert.IsFalse(lb.Items.Contains(name));
+                }
+            }
+        }
+
+        private ListBox SetUpListBoxForTest(string[] names)
+        {
+            CollectionNameEditor_Accessor target = new CollectionNameEditor_Accessor();
+            MongoDatabase database = Mock.Create<MongoDatabase>(Constructor.Mocked);
+
+            Mock.Arrange(() => database.GetCollectionNames()).Returns(names);
+
+            return target.BuildListBox(database);
+        }
+
+        private void NeedSealedClassMocks()
+        {
+            Assert.Inconclusive("Cannot test without paid-for mocking framework that allows mocking sealed classes.");
+        }
     }
 }
-
